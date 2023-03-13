4382dc509da7f801d10ee31e8f7b7c34b1048bad
erdenay <erdenay@Ubuntu>
2023-03-13 21:56:30 +0100

    all jenkinsfiles and mongodb edited

--- mongodb-app/frontend/script.js
+++ mongodb-app/frontend/script.js
@@ -1,5 +1,5 @@
 // Fetch the user data from the backend
-axios.get('http://backend.erdenayat.es:3002/users')
+axios.get('http://3.211.121.172:3002/users')
   .then(response => {
     // Populate the table with the user data
     const userTable = document.getElementById('user-table');
